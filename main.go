919597a79a7e0b130da76ade14b78c1f941b8c56
Vladimir Timofeev <vovkasm@gmail.com>
2017-11-10 16:41:26 +0300

    Add program version

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -9,14 +10,24 @@ import (
 	"github.com/robfig/cron"
 )
 
+var version = "master"
+
 var (
-	timeSpec string
+	timeSpec    string
+	showVersion bool
 )
 
 func main() {
 	flag.StringVar(&timeSpec, "spec", "", "Timespec in cron format")
+	flag.BoolVar(&showVersion, "version", false, "Show version")
 
 	flag.Parse()
+
+	if showVersion {
+		fmt.Printf("Version: %s\n", version)
+		os.Exit(0)
+	}
+
 	job := flag.Args()
 	if len(job) == 0 {
 		log.Println("Job is empty")
